5e04d115690783321819a4f9a0e5e47567506207
npasquier <nicolas.pasquier.cavalli@gmail.com>
2023-05-01 22:53:54 +0200

    ajout arrow scrowl down

--- script.js
+++ script.js
@@ -5,25 +5,16 @@
 API for jokes
 */
 
-var request = new XMLHttpRequest() 
-
-request.open('GET', 'https://v2.jokeapi.dev/joke/Dark?type=single', true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-    console.log(data.joke)
-  }
-
-
-
+var request = new XMLHttpRequest() ;
 
 function displayJoke() {
-  request.open('GET', 'https://v2.jokeapi.dev/joke/Dark?type=single', true)
+  request.open('GET', 'https://v2.jokeapi.dev/joke/Dark?type=single', true);
   request.onload = function () {
   // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
+  var data = JSON.parse(this.response);
     document.querySelector('#getJoke').innerHTML = data.joke;
   }
   
   request.send();
 };
+
--- script_nav.js
+++ script_nav.js
@@ -7,24 +7,29 @@ Display Menu upon Click on Image or Press 'v'
 
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === 'v') {
-      document.querySelector('nav').classList.toggle('hideHeader');
-    } else {
-      console.log(`"${e.key}" is not the right key. Try again :)`);
-    }
-  });
+  if (e.key === 'v') {
+    document.querySelector('nav').classList.toggle('hideHeader');
+  } else {
+    console.log(`"${e.key}" is not the right key. Try again :)`);
+  }
+});
 
-  document.querySelector("img").addEventListener("click", (e) => {
-      document.querySelector('nav').classList.toggle('hideHeader');
-    }
-  );
-
-  document.querySelector("img").addEventListener("touchstart", (e) => {
-    setTimeout( () => {document.querySelector('nav').classList.remove('hideHeader');} , 10000)
+document.querySelector("img").addEventListener("click", (e) => {
+    document.querySelector('nav').classList.toggle('hideHeader');
   }
 );
 
+document.querySelector("img").addEventListener("touchstart", (e) => {
+  setTimeout( () => {document.querySelector('nav').classList.remove('hideHeader');} , 4000)
+}
+);
 
-  
+/*
+* Display arrow with delay
+*/
 
+setTimeout(() => {
+  document.querySelector('.textArrow').classList.remove('hideArrow');
+  document.querySelector('.arrow').classList.remove('hideArrow');
+}, 3000);
 
