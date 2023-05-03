0eeb2432a0a0cafadbccf2f5e470cbfc48997112
npasquier <nicolas.pasquier.cavalli@gmail.com>
2023-05-03 22:48:57 +0200

    simplification of frame of website

--- script_nav.js
+++ script_nav.js
@@ -9,17 +9,19 @@ Display Menu upon Click on Image or Press 'v'
 document.addEventListener("keydown", (e) => {
   if (e.key === 'v') {
     document.querySelector('nav').classList.toggle('hideHeader');
+    document.querySelector('#favicon').href="./images/smile_inv.ico";
   } else {
     console.log(`"${e.key}" is not the right key. Try again :)`);
   }
 });
 
-document.querySelector("img").addEventListener("click", (e) => {
+document.querySelector(".profile-picture-img").addEventListener("click", (e) => {
     document.querySelector('nav').classList.toggle('hideHeader');
+    document.querySelector('#favicon').href="./images/smile_inv.ico";
   }
 );
 
-document.querySelector("img").addEventListener("touchstart", (e) => {
+document.querySelector(".profile-picture-img").addEventListener("touchstart", (e) => {
   setTimeout( () => {document.querySelector('nav').classList.remove('hideHeader');} , 4000)
 }
 );
@@ -28,8 +30,17 @@ document.querySelector("img").addEventListener("touchstart", (e) => {
 * Display arrow with delay
 */
 
-setTimeout(() => {
-  document.querySelector('.textArrow').classList.remove('hideArrow');
-  document.querySelector('.arrow').classList.remove('hideArrow');
-}, 3000);
+document.querySelector(".back-image").addEventListener("touchstart", (e) => {
+  document.querySelector('.first-flex-arrow').classList.toggle('hideArrow');
+  setTimeout( () => {document.querySelector('.first-flex-arrow').classList.add('hideArrow');} , 3000);
+}
+);
+
+document.querySelector(".back-image").addEventListener("click", (e) => {
+  document.querySelector('.first-flex-arrow').classList.toggle('hideArrow');
+  setTimeout( () => {document.querySelector('.first-flex-arrow').classList.add('hideArrow');} , 3000);
+}
+);
+
+
 
