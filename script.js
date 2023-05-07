101eb8144a029dfb5a2ac957a56f0651d068089c
npasquier <nicolas.pasquier.cavalli@gmail.com>
2023-05-07 14:24:16 +0200

    implement weather API

--- script.js
+++ script.js
@@ -18,3 +18,48 @@ function displayJoke() {
   request.send();
 };
 
+/*
+API for weather (with another get method)
+*/
+
+
+
+
+function displayWeather() {
+  const query = document.getElementById("cityInput").value;
+  if (query !== ''){
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=a9bc0664251a52a2051c28a3ce415070&units=metric';
+  fetch(url, {
+    "method": "GET",
+    })
+  .then(response => {
+    return response.json() ;
+  })
+  .then(function(data) {
+    const status = data.cod;
+    if (status==200){
+    const temp = data.main.temp;
+    const weather = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector('#getWeather').innerHTML = 'The current weather in ' + query + ' is '+  data.weather[0].description  + '<br>' +
+    'The temperature is of ' + temp + ' degrees Celcius. <br>'+
+    '<img src= '+ imageURL + '>';
+    ;
+    // console.log(data.cod);
+    // console.log(data.weather[0].description);
+    // console.log(data.main.temp);
+    // console.log(data);
+  }
+  else {
+    document.querySelector('#getWeather').innerHTML = 'The API does not recognize this city.. <br> (it might be because your city name is not written in english)' ;
+  }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+  }
+  if (query == '') {
+    document.querySelector('#getWeather').innerHTML = 'Don\'t forget to enter a city name :)';
+  }
+};
