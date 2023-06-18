baf364fd8ad6acf352af9c160b80b33d231c6c51
npasquier <nicolas.pasquier.cavalli@gmail.com>
2023-06-18 17:56:16 +0200

    new style with card

--- script.js
+++ script.js
@@ -1,65 +1,72 @@
-
-
-
 /*
 API for jokes
 */
 
-var request = new XMLHttpRequest() ;
+var request = new XMLHttpRequest();
 
 function displayJoke() {
-  request.open('GET', 'https://v2.jokeapi.dev/joke/Dark?type=single', true);
+  request.open("GET", "https://v2.jokeapi.dev/joke/Dark?type=single", true);
   request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-    document.querySelector('#getJoke').innerHTML = data.joke;
-  }
-  
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    document.querySelector("#getJoke").innerHTML = data.joke;
+  };
+
   request.send();
-};
+}
 
 /*
 API for weather (with another get method)
 */
 
-
-
-
 function displayWeather() {
   const query = document.getElementById("cityInput").value;
-  if (query !== ''){
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=a9bc0664251a52a2051c28a3ce415070&units=metric';
-  fetch(url, {
-    "method": "GET",
+  if (query !== "") {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      query +
+      "&appid=a9bc0664251a52a2051c28a3ce415070&units=metric";
+    fetch(url, {
+      method: "GET",
     })
-  .then(response => {
-    return response.json() ;
-  })
-  .then(function(data) {
-    const status = data.cod;
-    if (status==200){
-    const temp = data.main.temp;
-    const weather = data.weather[0].description;
-    const icon = data.weather[0].icon;
-    const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector('#getWeather').innerHTML = 'The current weather in ' + query + ' is '+  data.weather[0].description  + '<br>' +
-    'The temperature is of ' + temp + ' degrees Celcius. <br>'+
-    '<img src= '+ imageURL + '>';
-    ;
-    // console.log(data.cod);
-    // console.log(data.weather[0].description);
-    // console.log(data.main.temp);
-    // console.log(data);
+      .then((response) => {
+        return response.json();
+      })
+      .then(function (data) {
+        const status = data.cod;
+        if (status == 200) {
+          const temp = data.main.temp;
+          const weather = data.weather[0].description;
+          const icon = data.weather[0].icon;
+          const imageURL =
+            "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+          document.querySelector("#getWeather").innerHTML =
+            "The current weather in " +
+            query +
+            " is " +
+            data.weather[0].description +
+            "<br>" +
+            "The temperature is of " +
+            temp +
+            " degrees Celcius. <br>" +
+            "<img src= " +
+            imageURL +
+            ">";
+          // console.log(data.cod);
+          // console.log(data.weather[0].description);
+          // console.log(data.main.temp);
+          // console.log(data);
+        } else {
+          document.querySelector("#getWeather").innerHTML =
+            "The API does not recognize this city.. <br> (it might be because your city name is not written in english)";
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
-  else {
-    document.querySelector('#getWeather').innerHTML = 'The API does not recognize this city.. <br> (it might be because your city name is not written in english)' ;
+  if (query == "") {
+    document.querySelector("#getWeather").innerHTML =
+      "Don't forget to enter a city name :)";
   }
-  })
-  .catch(err => {
-    console.error(err);
-  });
-  }
-  if (query == '') {
-    document.querySelector('#getWeather').innerHTML = 'Don\'t forget to enter a city name :)';
-  }
-};
+}
--- script_nav.js
+++ script_nav.js
@@ -1,76 +1,70 @@
-
-
-
 /*
 Display Menu upon Click on Image or Press 'v' 
 */
 
-
 document.addEventListener("keydown", (e) => {
-  if (e.key === 'v') {
-    document.querySelector('nav').classList.toggle('hideHeader');
-    document.querySelector('#favicon').href="./images/smile_inv.ico";
+  if (e.key === "v") {
+    document.querySelector("#nav-playground").classList.toggle("hideHeader");
+    document.querySelector("#favicon").href = "./images/smile_inv.ico";
   } else {
     console.log(`"${e.key}" is not the right key. Try again :)`);
   }
 });
 
-document.querySelector(".profile-picture-img").addEventListener("click", (e) => {
-    document.querySelector('nav').classList.toggle('hideHeader');
-    document.querySelector('#favicon').href="./images/smile_inv.ico";
-  }
-);
+document
+  .querySelector(".profile-picture-img")
+  .addEventListener("click", (e) => {
+    document.querySelector("#nav-playground").classList.toggle("hideHeader");
+    document.querySelector("#favicon").href = "./images/smile_inv.ico";
+  });
 
-document.querySelector(".profile-picture-img").addEventListener("touchstart", (e) => {
-  setTimeout( () => {document.querySelector('nav').classList.remove('hideHeader');} , 4000)
-}
-);
+document
+  .querySelector(".profile-picture-img")
+  .addEventListener("touchstart", (e) => {
+    setTimeout(() => {
+      document.querySelector("#nav-playground").classList.remove("hideHeader");
+    }, 4000);
+  });
 
 /*
-* Display arrow with delay
-*/
+ * Animation
+ */
 
-let i=0;
-  
-document.querySelector(".back-image").addEventListener("touchstart", (e) => {
-  i++;
-  if (i >= 3){
-  setTimeout( () => {document.querySelector('.first-flex-arrow').classList.add('hideArrow');} , 2000);
-  i = 0;
-  return;
-};}
-);
-
-document.querySelector(".back-image").addEventListener("click", (e) => {
-  document.querySelector('.first-flex-arrow').classList.toggle('hideArrow');
-  setTimeout( () => {document.querySelector('.first-flex-arrow').classList.add('hideArrow');} , 3000);
-}
-);
-
-/* 
-* Animation
-*/
-
-const observer = new IntersectionObserver(entries => {
+const observer = new IntersectionObserver((entries) => {
   // Loop over the entries
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     // If the element is visible
     if (entry.isIntersecting) {
       // Add the animation class
-      entry.target.classList.add('div-opacity-transition');
-    }
-    else {
-      entry.target.classList.remove('div-opacity-transition');
+      entry.target.classList.add("div-opacity-transition");
+    } else {
+      entry.target.classList.remove("div-opacity-transition");
     }
   });
 });
 
-document.querySelectorAll('.div-opacity').forEach((i) => {
+document.querySelectorAll(".div-opacity").forEach((i) => {
   if (i) {
-      observer.observe(i);
+    observer.observe(i);
   }
 });
 
+/*
+ * Read more
+ */
 
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myReadMoreBtn");
 
-
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
