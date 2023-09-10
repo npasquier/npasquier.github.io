4f18c0e07311997cbc16f5744ad876104196ebcc
npasquier <nicolas.pasquier.cavalli@gmail.com>
2023-09-10 16:59:19 +0200

    fix keyboard activation in  playground

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ API for jokes
 
 var request = new XMLHttpRequest();
 
-var isFirstDarkJoke = false; 
+var isFirstDarkJoke = false;
 
 function displayJoke() {
-
   isFirstDarkJoke = true;
 
   request.open("GET", "https://v2.jokeapi.dev/joke/Dark?type=single", true);
@@ -22,14 +21,12 @@ function displayJoke() {
   }, 3000);
 
   setTimeout(() => {
-    document.querySelector("#darkJokeID").innerHTML = "Last one ! Promise  ! ;p";
+    document.querySelector("#darkJokeID").innerHTML =
+      "Last one ! Promise  ! ;p";
   }, 8000);
-
 
   request.send();
 }
-
-
 
 /*
 Dropdown nav btn
@@ -44,73 +41,88 @@ navLinks.forEach((l) => {
   });
 });
 
-
 /*
 Drum Kit
 */
 
-var numberOfDrumButtons= document.querySelectorAll(".drum").length;
+function handleKeyboardActivation() {
+  if (
+    document.getElementById("keyboard-active").innerText === "Activate Keyboard"
+  ) {
+    console.log("Keyboard was active");
+    document.getElementById("keyboard-active").innerText = "Disactivate Keyboard";
+  }
+  else if (
+    document.getElementById("keyboard-active").innerText === "Disactivate Keyboard"
+  ) {
+    console.log("Keyboard was inactive");
+    document.getElementById("keyboard-active").innerText = "Activate Keyboard";
+  };
+}
 
-
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 function makeSound(keyPressed) {
+  if (
+    document.getElementById("keyboard-active").innerText == "Disactivate Keyboard"
+  ) {
     switch (keyPressed) {
-        case 'a':
-            var tom1 = new Audio('./sounds/tom-1.mp3');
-            tom1.play();
-            break;
-        case 'z':
-            var tom2 = new Audio('./sounds/tom-2.mp3');
-            tom2.play();
-            break;
-        case 'e':
-            var tom3 = new Audio('./sounds/tom-3.mp3');
-            tom3.play();
-            break;
-        case 'r':
-            var tom4 = new Audio('./sounds/tom-4.mp3');
-            tom4.play();
-            break;
-        case 'i':
-            var snare = new Audio('./sounds/snare.mp3');
-            snare.play();
-            break;
-        case 'o':
-            var crash = new Audio('./sounds/crash.mp3');
-            crash.play();
-            break;
-        case 'p':
-            var kick = new Audio('./sounds/kick-bass.mp3');
-            kick.play();
-            break;
-        default: console.log(keyPressed);
-            break;
-    };
-} ;
+      case "a":
+        var tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "z":
+        var tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "e":
+        var tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "r":
+        var tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "i":
+        var snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+      case "o":
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+      case "p":
+        var kick = new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      default:
+        console.log(keyPressed);
+        break;
+    }
+  }
+}
 
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document
+    .querySelectorAll(".btn_drum")
+    [i].addEventListener("click", function () {
+      // var keyPressed = this.innerHTML ;
+      makeSound(this.innerHTML);
+      buttonAnimation(this.innerHTML);
+    });
+}
 
-for (let i = 0 ; i < numberOfDrumButtons ; i++) {
-     document.querySelectorAll(".btn_drum")[i].addEventListener("click", function (){
-        // var keyPressed = this.innerHTML ;
-        makeSound(this.innerHTML) ;
-        buttonAnimation(this.innerHTML);
+document.addEventListener("keydown", function (event) {
+  // var keyPressed = event.key ;
+  // console.log(event.key);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
-     });
-      
- };
-
- document.addEventListener('keydown', function (event) {
-    // var keyPressed = event.key ;
-    // console.log(event.key);
-    makeSound(event.key) ;
-    buttonAnimation(event.key);
- }
- );
-
- function buttonAnimation(currentKey) {
-   var activeButton = document.querySelector("."+currentKey);
-   activeButton.classList.add("pressed");
-   setTimeout(function () {
-    activeButton.classList.remove("pressed")
-   }, 100)
- }
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
--- script_nav.js
+++ script_nav.js
@@ -1,30 +1,3 @@
-/*
-Display Menu upon Click on Image or Press 'v' 
-*/
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "v") {
-    document.querySelector("#nav-playground").classList.toggle("hideHeader");
-    document.querySelector("#favicon").href = "./images/smile_inv.ico";
-  } else {
-    console.log(`"${e.key}" is not the right key. Try again :)`);
-  }
-});
-
-document
-  .querySelector(".profile-picture-img")
-  .addEventListener("mouseenter", (e) => {
-    document.querySelector("#nav-playground").classList.toggle("hideHeader");
-    document.querySelector("#favicon").href = "./images/smile_inv.ico";
-  });
-
-document
-  .querySelector(".profile-picture-img")
-  .addEventListener("touchstart", (e) => {
-    setTimeout(() => {
-      document.querySelector("#nav-playground").classList.remove("hideHeader");
-    }, 4000);
-  });
 
 /*
  * Animation
