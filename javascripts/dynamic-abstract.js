8b1815d00847b31d6b0646ef4cb6b91fc01f73e9
npasquier <nicolas.pasquier.cavalli@gmail.com>
2025-04-12 17:04:44 +0200

    Update with publication references

--- javascripts/dynamic-abstract.js
+++ javascripts/dynamic-abstract.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".abstract").forEach((elem) =>
     if (e.target.value === "decentralization") {
       document.getElementById("index").innerHTML = `
     <h1>Decentralization and consumer welfare with substitutes or complements.</h1>
-    <h3> <em> Review of Industrial Organization</em>, 2024, published online.</h3>
+    <h3> <em> Review of Industrial Organization</em>, 2024, Volume 64, pages 449–469.</h3>
     <p><small>DOI: <a href="https://link.springer.com/article/10.1007/s11151-023-09941-x"> https://link.springer.com/article/10.1007/s11151-023-09941-x</a></small></p>
     <p> <strong> Abstract: </strong> We study a vertically integrated producer (VIP) that supplies a downstream firm under price competition. The VIP may decentralize the final price decision to its downstream unit; the latter thereby ignores the effect of the output price on upstream sales. We find that decentralization benefits the VIP -- irrespective of whether the products are substitutes or complements. Decentralization also benefits the consumers when products are substitutes, but it harms them when the products are complements. Interestingly, when products are substitutes, decentralization decreases both output prices despite restoring a double margin on the downstream unit's sales.</p>
     <a href="./index.html"> [Back] </a>
@@ -32,7 +32,7 @@ document.querySelectorAll(".abstract").forEach((elem) =>
       document.getElementById("index").innerHTML = `
     <h1>Multiple agricultural risks and insurance -- Issues, perspectives and illustration for wine-growing.</h1>
     <h4> with Cécile Aubert, Yann Raineau and Marc Raynal. </h3>
-    <h3> <em>R&R at Review of Agricultural, Food and Environmental Studies</em>, Latest Version: September 2023.</h3>
+    <h3> <em>Review of Agricultural, Food and Environmental Studies, 2024, Volume 105, pages 371–391.</em></h3>
     <p><small>Draft link: <a href="https://doi.org/10.1007/s41130-024-00217-w"> https://doi.org/10.1007/s41130-024-00217-w </a></small></p>
     <br>
     <p> <strong> Abstract: </strong> We investigate to what extent crop insurance can address the multiple risks faced by
@@ -49,20 +49,27 @@ document.querySelectorAll(".abstract").forEach((elem) =>
     `;
     } else if (e.target.value === "amazon") {
       document.getElementById("index").innerHTML = `
-    <h1>Winners and Losers of Gatekeeper-Induced Consumer Preference Distortion in Promoting Personalized Pricing by Asymmetric Firms.</h1>
+    <h1>Winners and Losers of Gatekeeper-Induced Consumer Preference Distortion in Promoting Personalized Pricing.</h1>
     <h4> with Rosa Branca Esteves. </h3>
     <h3> <em>Submitted</em>, Latest Version: February 2024.</h3>
     <p><small>Draft link: <a href=""> ... </a></small></p>
     <br>
-    <p> <strong> Abstract: </strong> We present a model of duopoly competition in a marketplace with a Hotelling segment of consumers, where two business users (firms) have access to raw consumer data. The firms can choose between personalized prices (PP), using a costly personalized program device provided by the marketplace, or uniform prices at no additional cost. One firm has a higher level of experience in utilizing consumer data, resulting in a lower cost of price personalization (PP device cost). In order to promote its personalized program device, the marketplace may have an incentive to distort consumer preferences from a uniform to a triangular distribution. Our findings indicate that the marketplace is more likely to distort consumer preferences under specific conditions. This occurs when there is moderate asymmetry in experience between the firms and a high tariff for the program, or when there is weak asymmetry and a moderate program tariff. In these parameter regions, the distortion of consumer preferences negatively impact the profits of the sellers while benefiting the consumers. These insights contribute to a better understanding of the dynamics of digital marketplaces and have implications for policymakers and competition authorities.</p>
+    <p> <strong> Abstract: </strong> We develop a model of duopoly competition on a gatekeeper platform, where firms choose between personalized pricing (PP) and uniform pricing (UP). 
+Adopting PP requires firms to pay a per-consumer fee to the platform for access to pricing tools. We show that the platform actively reshapes market conditions to increase PP adoption.
+Starting from a setting with uniformly distributed consumer preferences (&beta; = 0), firms adopt PP only if fees are low.
+Anticipating this, the platform manipulates consumer preferences via biased recommendation algorithms -- like increased brand mix -- weakening consumer loyalty.
+This shifts the distribution toward a more triangular shape (&beta; &gt; 0), where more consumers cluster around the market center, intensifying price competition under UP.
+Firms therefore face greater pressure to use PP, ensuring that they pay the platform’s fee. However, the platform does not push brand mixing to the extreme (&beta; = 1). 
+Instead, it selects an intermediate level (&beta; &approx; 0.5) that maximizes its revenue: disloyalty is high enough to make PP attractive for firms but not so high that it erodes its profitability. This manipulation reduces firm profits and consumer surplus while boosting platform revenue. 
+Our findings shed light on hidden platform strategies under the current digital market regulation.</p>
     <a href="./index.html"> [Back] </a>
     `;
     } else if (e.target.value === "demand-uncertainty") {
       document.getElementById("index").innerHTML = `
     <h1>Two part tariff, demand uncertainty and risk aversion: From theory to experimental evidence.</h1>
     <h4> with Olivier Bonroy and Alexis Garapin. </h3>
-    <h3> <em>Accepted</em>, Latest Version: November 2024.</h3>
-    <p><small>Draft link: <a href=""> ... </a></small></p>
+    <h3> <em>Economics Letters, Volume 247, February 2025.</em></h3>
+    <p><small>Draft link: <a href="https://doi.org/10.1016/j.econlet.2025.112171"> https://doi.org/10.1016/j.econlet.2025.112171 </a></small></p>
     <br>
     <p> <strong> Abstract: </strong> In an uncertain demand environment, the per-unit price of a two-part tariff can have
 insurance properties, insofar as it transfers risk from risk-averse downstream firms to
@@ -70,27 +77,6 @@ less risk averse upstream firms, and compensation properties insofar as it compe
 downstream firms for lessened attractiveness of trade. Both properties have opposite
 effects such as the first one can increase double marginalization and the second one can
 mitigate it. This paper provides experimental evidence of both properties.</p>
-    <a href="./index.html"> [Back] </a>
-    `;
-    } else if (e.target.value === "colza") {
-      document.getElementById("index").innerHTML = `
-    <h1>Promotion of emission-based innovations in the biofuel sector.</h1>
-    <h4> with Stéphane Lemarié. </h3>
-    <h3> <em>New version available soon</em>, Latest Version: ...</h3>
-    <p><small>Draft link: <a href=""> ... </a></small></p>
-    <br>
-    <p> <strong> Abstract: </strong> Transitioning from fossil fuel to biofuel is a key strategy for reducing GHG emissions in energy-intensive sectors. While existing research has delved into how taxes, mandates, and other public interventions encourage this transition and stimulate cost-reducing innovations in biofuel production, the study of innovations aimed specifically at reducing biofuel emissions remains scant. Addressing this gap, our paper explores the impact of public interventions on the adoption of these emission-reducing innovations within the biofuel production. Through a model where an innovator markets such innovations to an industry which produces and blends biofuel with fossil fuel, we unveil how quota-based interventions fail to incentivize the industry towards adopting these innovations. Intriguingly, we then find that while an emission tax encourages the adoption of significant emission-reducing innovations, emission standards paradoxically favor only innovations yielding marginal improvements. Finally, we analyze the innovator's pricing strategy and its surplus sharing.  Our findings not only challenge conventional wisdom but also offers nuanced insights for policymakers aiming to refine regulatory measures to maximize the environmental benefits of biofuel innovation.</p>
-    <a href="./index.html"> [Back] </a>
-    `;
-    } else if (e.target.value === "survey") {
-      document.getElementById("index").innerHTML = `
-    <h1>Crop insurances and pesticide use: A survey of empirical results
-    with added focus on wine-growing.</h1>
-    <h4> with Cécile Aubert. </h3>
-    <h3> <em>New version available soon</em>, Latest Version: November 2023.</h3>
-    <p><small>Draft link: <a href=""> ... </a></small></p>
-    <br>
-    <p> <strong> Abstract: </strong> Crop insurance is a major component of the U.S. agricultural policy toolbox. On the contrary, only a small minority of E.U. farmers hold a subsidized insurance policy. We contrast the existing systems of subsidized insurance, that cover different risks. Interestingly, these different schemes give rise to different incentives for farmers to preserve the environment, in terms of adaptation, land use, and pesticide use. We focus on this latter aspect of the moral hazard problem and summarize existing empirical results. Apparently contradictory or inconclusive results can be related to the insurance system and crops under study. We discuss existing evidence about the impact on farmers' choices of green insurance -- an insurance contract conditional on pre-specified environmentally-friendly farmer behaviors. We offer a more in-depth survey for wine-growing, where pesticides are very inexpensive compared to the end-product, the extensive margin is quasi-null, and climatic and pest risks are correlated.</p>
     <a href="./index.html"> [Back] </a>
     `;
     } else if (e.target.value === "public-game") {
